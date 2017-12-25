e4d0ec99b598be4def9e3e5e3977c1c3053125c1
Josip Igrec <me@allypost.net>
2017-12-25 22:09:18 +0100

    Added basic routes

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,19 @@ use Slim\Http\Response;
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
+    /**
+     * @var \Slim\Router $router ;
+     */
+    $router = $this->router;
+
+    $postRoute = $router->pathFor('room:join.post');
+
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.phtml', compact('args', 'postRoute'));
 });
+
+$app->post('/join', function (Request $request, Response $response, array $args) {
+    $room = (int)$request->getParam('room');
+
+    return \Allypost\Api\Output::say($response, 'room join', compact('room'));
+})->setName('room:join.post');
