b84bba5f7074fcbec288848ddeb238efc41ba70c
Josip Igrec <me@allypost.net>
2017-12-26 06:29:07 +0100

    - When joining a room a cookie is now set which remembers the room for 8 hours - Created basic room view

--- src/routes.php
+++ src/routes.php
@@ -19,11 +19,40 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->view->render($response, 'pages/index.twig', $args);
 })->setName('room:join')->add($csrf);
 
+$app->get('/{room:\d{4}}', function (Request $request, Response $response, array $args) {
+    $room = (int)$args['room'];
+
+    return Output::say($response, 'room join', compact('room'));
+})->setName('room:view');
+
 $app->post('/join', function (Request $request, Response $response, array $args) {
     $room = (int)$request->getParam('room');
 
-    if ($room < 0 || $room > 9999)
+    if ($room < 0 || $room > 9999) {
         return Output::err($response, 'room join', ['Invalid room number supplied']);
+    }
 
-    return Output::say($response, 'room join', compact('room'));
+    $paddedRoom = str_pad((string)$room, 4, '0', STR_PAD_LEFT);
+
+    /**
+     * @var \Slim\Router $router
+     * @var \Slim\Http\Cookies $cookie
+     */
+    $router = $this->router;
+    $cookie = $this->cookie;
+
+    $cookie->set('room-number', [
+        'value' => $paddedRoom,
+        'expires' => 60 * 60 * 8 + time(),
+        'secure' => true,
+        'httponly' => true,
+        'path' => '/',
+        'host' => $request->getServerParam('SERVER_NAME')
+    ]);
+
+    return $response
+        ->withHeader('Set-Cookie', $cookie->toHeaders())
+        ->withRedirect(
+            $router->pathFor('room:view', ['room' => $paddedRoom])
+        );
 })->setName('room:join.post')->add($csrf);
