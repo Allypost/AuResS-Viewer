a41e2e283338409a70935d9394d95b79a4814b6e
Josip Igrec <me@allypost.net>
2017-12-26 05:10:27 +0100

    Added CSRF to join routes

--- src/routes.php
+++ src/routes.php
@@ -5,13 +5,21 @@ use Slim\Http\Response;
 
 // Routes
 
+/**
+ * @var \Slim\Container $container
+ * @var Slim\Csrf\Guard $csrf
+ */
+
+$container = $app->getContainer();
+$csrf = $container->get('csrf');
+
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Render index view
     return $this->view->render($response, 'pages/index.twig', $args);
-});
+})->setName('room:join')->add($csrf);
 
 $app->post('/join', function (Request $request, Response $response, array $args) {
     $room = (int)$request->getParam('room');
 
     return \Allypost\Api\Output::say($response, 'room join', compact('room'));
-})->setName('room:join.post');
+})->setName('room:join.post')->add($csrf);
